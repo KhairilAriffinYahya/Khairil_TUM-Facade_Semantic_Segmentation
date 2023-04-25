10b545ba0d7ae57b5a84616105f125f92e33fedf
KhairilAriffinYahya <khairilariffin94@gmail.com>
2023-04-25 22:30:31 +0800

    Update runlines.py

--- Unused files/runlines.py
+++ Unused files/runlines.py
@@ -2,3 +2,6 @@ python train_trial6.py --model pointnet2_sem_seg_trial --test_area 'DEBY_LOD2_49
 
 cd /Khairil_code/Khairil_PN2_experiment/experiment
 cd /home/tum-facade_Khairil
+
+rm -r Khairil_PN2_experiment/
+git clone https://github.com/KhairilAriffinYahya/Khairil_PN2_experiment.git
