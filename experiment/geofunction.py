68cf9a22d65297cf386e9620de2001297443df74
KhairilAriffinYahya <khairilariffin94@gmail.com>
2023-05-02 17:30:42 +0800

    Create geofunction.py

---
+++ experiment/geofunction.py
@@ -0,0 +1,109 @@
+import argparse
+import os
+import torch
+import datetime
+import logging
+from pathlib import Path
+import sys
+import importlib
+import shutil
+import provider
+import numpy as np
+import time
+from tqdm import tqdm
+import laspy
+import glob
+from collections import Counter
+from torch.utils.data import Dataset, DataLoader, random_split
+import matplotlib.pyplot as plt
+import time
+import open3d as o3d
+import pickle
+
+def PCA(data, correlation=False, sort=True):
+    average_data = np.mean(data, axis=0)  # 求 NX3 向量的均值
+    decentration_matrix = data - average_data  # 去中心化
+    H = np.dot(decentration_matrix.T, decentration_matrix)  # 求解协方差矩阵 H
+    eigenvectors, eigenvalues, eigenvectors_T = np.linalg.svd(H)  # SVD求解特征值、特征向量
+    # 屏蔽结束
+
+    if sort:
+        sort = eigenvalues.argsort()[::-1]  # 降序排列
+        eigenvalues = eigenvalues[sort]  # 索引
+        eigenvectors = eigenvectors[:, sort]
+
+    return eigenvalues, eigenvectors
+
+
+def collFeatures(pcd, length, size=0.8):
+    pcd_tree = o3d.geometry.KDTreeFlann(pcd)  # set a kd tree for tha point cloud, make searching faster
+    normals = []
+    llambda = []
+    lp = []
+    lo = []
+    lc = []
+    non_idx = []
+    # print(point_cloud_o3d)  #geometry::PointCloud with 10000 points.
+    print(length)  # 10000
+    for i in range(length):
+        # search_knn_vector_3d， input[point，x]      returns [int, open3d.utility.IntVector, open3d.utility.DoubleVector]
+        [_, idx, _] = pcd_tree.search_radius_vector_3d(pcd.points[i], size)
+        # asarray is the same as array  but asarray will save the memeory
+        k_nearest_point = np.asarray(pcd.points)[idx,
+                          :]  # find the surrounding points for each point, set them as a curve and use PCA to find the normal
+        lamb, v = PCA(k_nearest_point)
+        if len(k_nearest_point) == 1:
+            non_idx.append(i)  # record the index that has no knn point
+            p = 0
+            o = 0
+            c = 0
+        else:
+            p = (lamb[1] - lamb[2]) / lamb[0]  # calculate features based on eigenvalues
+            o = pow(lamb[0] * lamb[1] * lamb[2], 1.0 / 3.0)
+            c = lamb[2] / sum(lamb)
+        normals.append(v[:, 1])
+        llambda.append(lamb)
+        lp.append(p)
+        lo.append(o)
+        lc.append(c)
+    return np.array(normals), np.array(llambda), np.array(lp).reshape(length, -1), np.array(lo).reshape(length,-1), np.array(lc).reshape(length, -1), np.array(non_idx)
+
+
+def downsamplingPCD(pcd, dataset):
+    #Downsample
+    downpcd = pcd.voxel_down_sample(voxel_size=0.05)
+    downsampled_points = np.asarray(downpcd.points)
+    downsampled_labels = np.asarray(downpcd.get_point_attr("labels"))
+
+    #Update dataset
+    dataset.room_points = [downsampled_points]
+    dataset.room_labels = [downsampled_labels]
+    dataset.room_idxs = np.array([0])
+
+    #Update pcd after downsampling
+    all_points = np.vstack(dataset.room_points)
+    all_labels = np.concatenate(dataset.room_labels)
+    pcd_update = o3d.geometry.PointCloud(o3ddevice)
+    pcd_update.points = o3d.utility.Vector3dVector(all_points)
+
+    return pcd_update, all_points, all_labels, dataset
+
+def createPCD(dataset):
+    # Concatenate room_points and room_labels from all rooms
+    all_points = np.vstack(dataset.room_points)
+    all_labels = np.concatenate(dataset.room_labels)
+
+    # Create an Open3D point cloud object
+    pcd = o3d.geometry.PointCloud()
+
+    # Set the point positions using all_points
+    pcd.points = o3d.utility.Vector3dVector(all_points)
+
+    # Set the point labels using all_labels
+    all_labels_np = all_labels.reshape(-1, 1)  # Reshape the labels to have shape (N, 1)
+
+    # Create a tensor for point labels and assign it to the custom attribute 'labels'
+    labels_tensor = o3d.core.Tensor(all_labels_np, dtype=o3d.core.Dtype.Int32)
+    pcd.set_point_attr("labels", labels_tensor)
+
+    return pcd, all_points, all_labels
